7e5fcfb9a49569f684e1eb99c8927d271535db3d
Omar Pino <matato_357@hotmail.com>
2020-08-29 02:52:01 -0700

    All the charts working

---
+++ src/components/LineChart.vue
@@ -0,0 +1,45 @@
+<!-- Copyright (c) 2020. Omar Pino. All Rights Reserved. -->
+<script>
+import Controller from "@/../lib/controller";
+import { Line, mixins } from "vue-chartjs";
+const {reactiveProp} = mixins
+
+// import other components you use here...
+
+class LineChartController extends Controller {
+  constructor(name, subComponentList = []) {
+    super(name, subComponentList);
+    this.extends = Line;
+
+    this.vm = {
+      someData: "Hello world",
+      
+    };
+    this.props = {
+      // props are passed in when using this component
+      chartData: {},
+      options: {}
+    };
+    this.mixins = [reactiveProp]
+  }
+
+  vue_mounted() {
+    this.renderChart(this.chartData, this.options);
+  }
+}
+
+export default new LineChartController(
+  "lineChart" /* , { subComponent, anotherComponent } */
+);
+</script>
+<style scoped>
+/*
+    Add "scoped" attribute to limit CSS to this component only <style scoped>
+    styles that are specific to this component only, not sub-children
+    */
+.component-style {
+  display: flex;
+  height: 20vh;
+  width: 100%;
+}
+</style>
--- src/views/Chart.vue
+++ src/views/Chart.vue
@@ -1,34 +1,85 @@
 <template>
   <div class="about">
     <h1>Chart</h1>
-
+    <barChart v-if="loaded" :chartData="chartData" :options="options"></barChart>
+    <lineChart v-if="loaded" :chartData="chartData" :options="options"></lineChart>
+    <pieChart v-if="loaded" :chartData="chartData" :options="options"></pieChart>
+    <bubbleChart v-if="loaded" :chartData="chartData" :options="options"></bubbleChart>
+    <button @click="loadTelemetryData">Load</button>
   </div>
 </template>
 <script>
-    // typical Vue component controller defined and registered here
-    import Controller from '@/../lib/controller'
+// typical Vue component controller defined and registered here
+import Controller from "@/../lib/controller";
+import barChart from "@/components/BarChart.vue";
+import lineChart from "@/components/LineChart.vue";
+import pieChart from "@/components/PieChart.vue";
+import bubbleChart from "@/components/BubbleChart.vue";
+import TelemetryRecord from "@/../lib/TelemetryRecord";
 
-    class AdminController extends Controller {
+class AdminController extends Controller {
+  constructor(name, subComponentList = []) {
+    super(name, subComponentList);
+    this.vm = {
+      loaded: false,
+      savedRecords: [],
+      chartData: {
+        labels: ["BuildId", "PlayerId", "PositionX", "PositionY", "Action"],
+        datasets: []
+      },
+      options: {
+        responsive: true,
+        maintainAspectRatio: false
+      }
+    };
+    this.props = {};
 
-        constructor( name, subComponentList = []) {
-            super( name, subComponentList );
-            this.vm = {
-                buildId: 1234,
-                playerId: 0,
-                pos: {x: 0, y:0},
-                action: 0
-            }
-            this.props = {};
-        }
+    
+    this.injectActions(["updateRecord"]);
+    this.injectActions(["getRecords"]);
+    this.injectActions(["setTMRecords"]);
+    this.injectActions(["clearRecords"]);
+    this.injectGetters(["getCurrentRecord"]);
+    this.injectGetters(["getTelemetryArray"]);
+    this.injectGetters(["getStringRecords"]);
+  }
 
-        saveRec(){
-          //Save in firebase
-        }
-    }
+  loadTelemetryData() {
+    this.clearRecords();
+    this.savedRecords = [];
+    return new Promise((resolve, reject) => {
+      this.getRecords();
+      try {
+        JSON.parse(this.getStringRecords)
+      } catch (error) {
+        console.log("Press the load button again. The promise had not finished retreving data")
+        return;
+      }
+      let array = JSON.parse(this.getStringRecords);
+      let tmArray = [];
+      array.forEach(element => {
+        let td = new TelemetryRecord();
+        td.data = element;
+        td.count = element.count;
+        tmArray.push(td);
+      });
+      this.setTMRecords(tmArray)
+      this.chartData.datasets = []
+      tmArray.forEach(element =>{
+        let newDataset = {}
+        newDataset.label = "player " + element.data.playerId;
+        newDataset.backgroundColor = "#f87979";
+        newDataset.data = [element.data.buildId,element.data.playerId,element.data.pos.x,element.data.pos.y,element.data.action];
+        this.savedRecords.push(element.asString());
+        this.chartData.datasets.push(newDataset);
+      })
+      this.loaded = true;
+    });
+  }
+}
 
-    // export a definition for this view
-    export default new AdminController('telemetryAdmin', {});
-
+// export a definition for this view
+export default new AdminController("telemetryAdmin", { barChart, lineChart, pieChart, bubbleChart });
 </script>
 <style scoped>
 #app {
